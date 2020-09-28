4f0acde1f9c00f4a28c876e1c003eab8271c5ed8
lz845960053 <lz845960053@163.com>
2020-09-28 18:30:56 +0800

    课程列表

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -29,6 +29,43 @@ export default{
 			method: 'put',
 			data:courseInfo,
 		})
+	},
+  //根据课程id 获取最终发布课程信息
+  getPublishCourseInfo(courseId){
+    return request({
+      url: `/eduservice/course/getPublishCourseInfo/${courseId}`,
+      method: 'get'
+    })
+  },
+  //课程最终发布
+  publishCourse(courseId){
+	  return request({
+      url:`/eduservice/course/publishCourse/${courseId}`,
+      method:'put',
+    })
+  },
+  //获取课程列表
+  getListCourse(){
+	  return request({
+      url:`/eduservice/course/getCourseList`,
+      method:'get'
+    })
+  },
+  //分页获取课程列表
+  getPageListCourse(page,limit,courseQuery){
+    return request({
+      url:`/eduservice/course/getCoursePageList/${page}/${limit}`,
+      method:'post',
+      data:courseQuery,
+    })
+  },
+  //删除课程
+  removeCourseById(courseId){
+	  return request({
+      url:`/eduservice/course/deleteCourseById/${courseId}`,
+      method:'delete'
+    })
+  }
 
-	}
+
 }
